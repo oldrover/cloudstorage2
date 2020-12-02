60399b83fa2ed26276a1a6628626b4d05a75126a
oldrover <oldrover@web.de>
2020-12-02 15:08:36 +0100

    renamed Notes to Note

--- src/main/java/com/udacity/jwdnd/course1/cloudstorage/model/Note.java
+++ src/main/java/com/udacity/jwdnd/course1/cloudstorage/model/Note.java
@@ -1,12 +1,12 @@
 package com.udacity.jwdnd.course1.cloudstorage.model;
 
-public class Notes {
+public class Note {
     private Integer noteId;
     private String noteTitle;
     private String noteDescription;
     private Integer userId;
 
-    public Notes(Integer noteId, String noteTitle, String noteDescription, Integer userId) {
+    public Note(Integer noteId, String noteTitle, String noteDescription, Integer userId) {
         this.noteId = noteId;
         this.noteTitle = noteTitle;
         this.noteDescription = noteDescription;
