6381672cddb3887ccaca00457b293bb38b0e687e
oldrover <oldrover@web.de>
2020-12-02 11:10:40 +0100

    added SecurityConfig

---
+++ src/main/java/com/udacity/jwdnd/course1/cloudstorage/config/SecurityConfig.java
@@ -0,0 +1,13 @@
+package com.udacity.jwdnd.course1.cloudstorage.config;
+
+import com.udacity.jwdnd.course1.cloudstorage.service.AuthenticationService;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private AuthenticationService authenticationService;
+
+    public SecurityConfig(AuthenticationService authenticationService) {
+        this.authenticationService = authenticationService;
+    }
+}
