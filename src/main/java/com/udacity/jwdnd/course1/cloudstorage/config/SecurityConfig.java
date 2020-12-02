9f235f11d7e2e03c0f5f8cb793f20129c93e5bb3
oldrover <oldrover@web.de>
2020-12-02 11:16:47 +0100

    updated SecurityConfig config methods

--- src/main/java/com/udacity/jwdnd/course1/cloudstorage/config/SecurityConfig.java
+++ src/main/java/com/udacity/jwdnd/course1/cloudstorage/config/SecurityConfig.java
@@ -1,6 +1,8 @@
 package com.udacity.jwdnd.course1.cloudstorage.config;
 
 import com.udacity.jwdnd.course1.cloudstorage.service.AuthenticationService;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
@@ -9,5 +11,25 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     public SecurityConfig(AuthenticationService authenticationService) {
         this.authenticationService = authenticationService;
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.authenticationProvider(this.authenticationService);
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/signup", "/css/**", "/js/**").permitAll()
+                .anyRequest().authenticated();
+
+        http.formLogin()
+                .loginPage("/login")
+                .permitAll();
+
+        http.formLogin()
+                .defaultSuccessUrl("/home", true);
+
     }
 }
