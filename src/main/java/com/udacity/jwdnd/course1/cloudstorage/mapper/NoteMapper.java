2f55dfc05720f3b3bb7011571cf25ec55c038cb7
oldrover <oldrover@web.de>
2020-12-04 09:13:14 +0100

    added deleteNote method to NoteMapper

--- src/main/java/com/udacity/jwdnd/course1/cloudstorage/mapper/NoteMapper.java
+++ src/main/java/com/udacity/jwdnd/course1/cloudstorage/mapper/NoteMapper.java
@@ -1,10 +1,7 @@
 package com.udacity.jwdnd.course1.cloudstorage.mapper;
 
 import com.udacity.jwdnd.course1.cloudstorage.model.Note;
-import org.apache.ibatis.annotations.Insert;
-import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Options;
-import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.*;
 
 import java.util.List;
 
@@ -16,4 +13,7 @@ public interface NoteMapper {
     @Insert("INSERT INTO NOTES(notetitle, notedescription, userid) VALUES(#{noteTitle}, #{noteDescription}, #{userId})")
     @Options(useGeneratedKeys = true, keyProperty = "noteId")
     int insertNote(Note note);
+
+    @Delete("DELETE * FROM NOTES WHERE noteId = #{noteId}")
+    int deleteNote(Integer noteId);
 }
