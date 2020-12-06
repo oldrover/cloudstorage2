8c90bd0b5c0eab96555e40737e08a4806a57df8d
oldrover <oldrover@web.de>
2020-12-06 16:16:49 +0100

    added FileDataMapper

---
+++ src/main/java/com/udacity/jwdnd/course1/cloudstorage/mapper/FileDataMapper.java
@@ -0,0 +1,19 @@
+package com.udacity.jwdnd.course1.cloudstorage.mapper;
+
+import com.udacity.jwdnd.course1.cloudstorage.model.FileData;
+import org.apache.ibatis.annotations.*;
+
+import java.util.List;
+
+@Mapper
+public interface FileDataMapper {
+    @Select("SELECT * FROM FILES WHERE userid = #{userId}")
+    List<FileData> getFile(Integer userId);
+
+    @Insert("INSERT INTO FILES(filename, contenttype, filesize, userid, filedata) VALUES(#{fileName}, #{contentType}, #{fileSize}, #{userId}, #{file})")
+    @Options(useGeneratedKeys = true, keyProperty = "fileId")
+    int insertFile(FileData fileData);
+
+    @Delete("DELETE FROM FILES WHERE fileId = #{fileId}")
+    int deleteFile(Integer fileId);
+}
