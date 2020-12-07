57e77cdfd032cf867904ea5c7ae0f9c9fab4e959
oldrover <oldrover@web.de>
2020-12-07 10:25:10 +0100

    added mapping for /file

--- src/main/java/com/udacity/jwdnd/course1/cloudstorage/controller/HomeController.java
+++ src/main/java/com/udacity/jwdnd/course1/cloudstorage/controller/HomeController.java
@@ -1,6 +1,7 @@
 package com.udacity.jwdnd.course1.cloudstorage.controller;
 
 import com.udacity.jwdnd.course1.cloudstorage.model.CredentialForm;
+import com.udacity.jwdnd.course1.cloudstorage.model.FileData;
 import com.udacity.jwdnd.course1.cloudstorage.model.NoteForm;
 import com.udacity.jwdnd.course1.cloudstorage.model.User;
 import com.udacity.jwdnd.course1.cloudstorage.service.CredentialService;
@@ -11,8 +12,11 @@ import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.view.RedirectView;
+
+import java.io.IOException;
 
 @Controller
 @RequestMapping("/home")
@@ -34,6 +38,7 @@ public class HomeController {
     @GetMapping
     public String getHomePage(Authentication auth, NoteForm noteForm, CredentialForm credentialForm, Model model) {
         User user = userService.getUser(auth.getName());
+        model.addAttribute("files", this.fileService.getFiles(user.getUserId()));
         model.addAttribute("notes", this.noteService.getNotes(user.getUserId()));
         model.addAttribute("credentials", this.credentialService.getCredentials(user.getUserId()));
         return "home";
@@ -44,6 +49,14 @@ public class HomeController {
         User user = userService.getUser(auth.getName());
 
         switch (action) {
+            case "upfile":
+
+                break;
+
+            case "delf":
+                this.fileService.deleteFile(id);
+                break;
+
             case "addorupn":
                 if(noteForm.getNoteId() == null) {
                     this.noteService.addNote(user, noteForm);
@@ -52,9 +65,11 @@ public class HomeController {
                     this.noteService.updateNote(noteForm);
                 }
                 break;
+
             case "deln":
                 this.noteService.deleteNote(id);
                 break;
+
             case "addorupc":
                 if(credentialForm.getCredentialId() == null) {
                     this.credentialService.addCredential(user, credentialForm);
@@ -63,19 +78,34 @@ public class HomeController {
                     this.credentialService.updateCredential(credentialForm);
                 }
                 break;
+
             case "delc":
                 this.credentialService.deleteCredential(id);
                 break;
-
-            default:
 
         }
         model.addAttribute("files", this.fileService.getFiles(user.getUserId()));
         model.addAttribute("notes", this.noteService.getNotes(user.getUserId()));
         model.addAttribute("credentials", this.credentialService.getCredentials(user.getUserId()));
-        return "redirect:/home";
+        return "home";
 
 
+    }
+
+    @PostMapping("/file")
+    public String uploadFile(@RequestParam("fileUpload") MultipartFile fileUpload, Authentication auth,NoteForm noteForm, CredentialForm credentialForm, Model model) throws IOException {
+        User user = userService.getUser(auth.getName());
+        if(fileUpload.isEmpty()) {
+            model.addAttribute("success",false);
+            model.addAttribute("message","No file selected to upload!");
+            return "result";
+
+        }
+        fileService.addFile(new FileData(null,fileUpload.getOriginalFilename(),fileUpload.getContentType(),Long.toString(fileUpload.getSize()),user.getUserId(),fileUpload.getBytes()));
+        model.addAttribute("files", this.fileService.getFiles(user.getUserId()));
+        model.addAttribute("notes", this.noteService.getNotes(user.getUserId()));
+        model.addAttribute("credentials", this.credentialService.getCredentials(user.getUserId()));
+        return "home";
     }
 
 }
