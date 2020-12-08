7579689a6fe7d50346dcc3d85a34c4a057afd689
oldrover <oldrover@web.de>
2020-12-08 08:24:16 +0100

    added SignupPage for testing

---
+++ src/test/java/com/udacity/jwdnd/course1/cloudstorage/SignupPage.java
@@ -0,0 +1,56 @@
+package com.udacity.jwdnd.course1.cloudstorage;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class SignupPage {
+
+    @FindBy(id= "inputFirstName")
+    private WebElement inputFirstName;
+
+    @FindBy(id= "inputLastName")
+    private WebElement inputLastName;
+
+    @FindBy(id= "inputUsername")
+    private WebElement inputUsername;
+
+    @FindBy(id= "inputPassword")
+    private WebElement inputPassword;
+
+    @FindBy(id= "submitButton")
+    private WebElement submitButton;
+
+    @FindBy(id= "loginLink")
+    private WebElement loginlink;
+
+    public SignupPage(WebDriver webDriver) {
+        PageFactory.initElements(webDriver, this);
+
+    }
+
+    public void setFirstName(String firstName) {
+        inputFirstName.sendKeys(firstName);
+    }
+    public void setLastName(String lastName) {
+        inputLastName.sendKeys(lastName);
+    }
+
+    public void setUsername(String username) {
+        inputUsername.sendKeys(username);
+    }
+
+    public void setPassword(String password) {
+        inputPassword.sendKeys(password);
+    }
+
+    public void clickSubmitButton() {
+        submitButton.click();
+    }
+
+    public void clickLoginLink() {
+        loginlink.click();
+    }
+
+}
